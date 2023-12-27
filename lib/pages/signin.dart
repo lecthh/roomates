9d71935952748125bf79adda1e9b3254c4383a8b
lecthh <dev.lecth@gmail.com>
2023-12-27 17:36:38 +0800

    done signin

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -10,14 +10,22 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
+  final signInFormKey = GlobalKey<FormState>();
+  String name = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: const Color(0xffFFC508),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -58,7 +66,128 @@ class _SignInState extends State<SignIn> {
                     ],
                   )
                 ],
-              )
+              ),
+              Form(
+                key: signInFormKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16, bottom: 16),
+                      child: Text(
+                        "Welcome\nBack!",
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black,
+                          fontSize: 32
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StrokeText(
+                          text: "Username",
+                          strokeWidth: 2,
+                          textStyle: GoogleFonts.inter(
+                            color: const Color(0xffFDF9F1),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        const SizedBox(height: 8,),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            fillColor: Color(0xffFDF9F1),
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero,
+                              borderSide: BorderSide(
+                                width: 1.0
+                              )
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero,
+                              borderSide: BorderSide(
+                                width: 1.0
+                              )
+                            )
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 24,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StrokeText(
+                          text: "Password",
+                          strokeWidth: 2,
+                          textStyle: GoogleFonts.inter(
+                            color: const Color(0xffFDF9F1),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        const SizedBox(height: 8,),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            fillColor: Color(0xffFDF9F1),
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero,
+                              borderSide: BorderSide(
+                                width: 1.0
+                              )
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero,
+                              borderSide: BorderSide(
+                                width: 1.0
+                              )
+                            )
+                          ),
+                        ),
+                        const SizedBox(height: 24,),
+                        GestureDetector(
+                          onTap: () {},
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: 53,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xff2CC081),
+                                border: Border.all(
+                                  width: 1
+                                ),
+                              ),
+                              child: Center(
+                                child: StrokeText(
+                                  text: "Sign in",
+                                  strokeWidth: 2,
+                                  textStyle: GoogleFonts.inter(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                )
+                              ),
+                            ),
+                          )
+                        ),
+                        const SizedBox(height: 96),
+                        const Center(
+                          child: Text(
+                            "Already have an account? Sign up"
+                          ),
+                        ),
+                        const SizedBox(height: 96,)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
